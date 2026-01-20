8d2fb36f6c9bd5533548db83786e6b97afd8fb96
Ajinkya <ajinkyasultane@gmail.com>
2026-01-20 11:01:30 +0530

    Solve find a Factor Problem

---
+++ src/Conditional_Assignment/Factor.java
@@ -0,0 +1,21 @@
+package Conditional_Assignment;
+
+import java.util.Scanner;
+
+public class Factor {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number for find a factorial");
+
+        int num = sc.nextInt();
+
+        for (int i = 1; i <= num ; i++)
+        {
+            if (num % i == 0 )
+            {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
