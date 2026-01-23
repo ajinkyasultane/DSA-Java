021cd5e9b23074e9d4fdfbc1c504cc22ad7b515c
Ajinkya <ajinkyasultane@gmail.com>
2026-01-23 18:44:17 +0530

    Solve a Problem for Check a prime no

---
+++ src/AssignmentFunction/CheckPrime.java
@@ -0,0 +1,34 @@
+package AssignmentFunction;
+
+import java.util.Scanner;
+
+public class CheckPrime {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a No check prime or not");
+        int no = sc.nextInt();
+
+        boolean ans = isprime(no);
+        System.out.println(ans);
+
+    }
+    static boolean isprime(int no)
+    {
+        if (no <=1)
+        {
+            return false;
+        }
+        int c = 2;
+        while (c*c <= no)
+        {
+            if (no % c == 0)
+            {
+                return false;
+            }
+            c++;
+        }
+        return c*c > no;
+    }
+
+}
