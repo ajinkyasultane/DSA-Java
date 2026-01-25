a6e2dd56db53923965889f470b6223c6c56be534
Ajinkya <ajinkyasultane@gmail.com>
2026-01-25 12:13:54 +0530

    Solve a Program to swap element in array

---
+++ src/Arrays/SwapArrayElement.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class SwapArrayElement {
+    public static void main(String[] args)
+    {
+        int[] arr = {1,4,5,6,7};
+        System.out.println(Arrays.toString(arr));
+        swap(arr,0,3);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void  swap(int[] arr , int index1, int index2)
+    {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
