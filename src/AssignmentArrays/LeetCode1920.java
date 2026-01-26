e69b8d173ecc903d07b4f32167ceb0ad63ef8f65
Ajinkya <ajinkyasultane@gmail.com>
2026-01-26 10:50:46 +0530

    Solve a Leetcode 1920 problem with O(n) complexity

---
+++ src/AssignmentArrays/LeetCode1920.java
@@ -0,0 +1,23 @@
+package AssignmentArrays;
+
+import java.util.Arrays;
+
+public class LeetCode1920 {
+    public static void main(String[] args)
+    {
+        int[] arr = {0,2,1,5,3,4};
+        System.out.println(Arrays.toString(buildArray(arr)));
+
+    }
+    static int[] buildArray(int[] nums)
+    {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            ans[i]=nums[nums[i]];
+        }
+        return ans;
+    }
+}
