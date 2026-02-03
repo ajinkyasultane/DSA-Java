eb0a3ad56017e8044f0df5bc6dcdabd77d156c99
Ajinkya <ajinkyasultane@gmail.com>
2026-02-03 08:22:28 +0530

    Solve letcode BS 33 no problem

---
+++ src/BinarySearch/RBSLeet33.java
@@ -0,0 +1,79 @@
+package BinarySearch;
+
+public class RBSLeet33 {
+    public static void main(String[] args)
+    {
+        int[] arr = {4,5,6,7,5,4,3,2};
+        System.out.println(findpivot(arr));
+
+    }
+    static int search(int[] nums, int target)
+    {
+        int pivot = findpivot(nums);
+
+        if (pivot == -1)
+        {
+            return binarysearch(nums,target,0, nums.length -1);
+        }
+        if (nums[pivot] == target)
+        {
+            return pivot;
+        }
+        if (target >= nums[pivot])
+        {
+            return binarysearch(nums,target,0,pivot -1);
+        }
+        return binarysearch(nums, target, pivot+1, nums.length-1);
+    }
+    static int binarysearch(int[] arr, int target,int start , int end)
+    {
+        while (start <= end)
+        {
+            int mid = start + (end - start) /2;
+            if (target < arr[mid])
+            {
+                end = mid -1;
+            }
+            else if (target > arr[mid])
+            {
+                start = mid +1;
+            }
+            else
+            {
+                return mid;
+            }
+
+        }
+        return -1;
+    }
+
+    static int findpivot(int[] arr)
+    {
+        int start  =0;
+        int end = arr.length -1;
+
+        while(start <= end)
+        {
+            int mid = start + (end - start) /2;
+
+            if (mid < end && arr[mid] > arr[mid + 1])
+            {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid -1])
+            {
+                return mid -1;
+            }
+            if (arr[mid] <= arr[start])
+            {
+                end = mid -1;
+            }
+            else
+            {
+                start = mid +1;
+            }
+
+        }
+        return -1;
+    }
+}
