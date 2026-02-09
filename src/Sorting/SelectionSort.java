d2d9a170df7ff1d43b7eb768ee6eace8d15b2a77
Ajinkya <ajinkyasultane@gmail.com>
2026-02-09 08:07:34 +0530

    Learn selection sort

---
+++ src/Sorting/SelectionSort.java
@@ -0,0 +1,45 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args)
+    {
+        int[] arr = {5,4,3,2,1};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void selection(int[] arr)
+    {
+        for(int i = 0; i < arr.length ; i++)
+        {
+        int last = arr.length -i-1;
+        int maxindex = getmaxindex(arr, 0, last);
+
+        swap(arr, maxindex, last);
+
+        }
+    }
+
+    static int getmaxindex(int[] arr , int start , int last)
+    {
+        int max = start;
+
+        for (int i =0 ; i <= last; i++)
+        {
+            if (arr[max] < arr[i])
+            {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    static void swap(int[] arr , int first , int second)
+    {
+        int temp = arr[first];
+        arr[first]  = arr[second];
+        arr[second] = temp;
+    }
+
+}
