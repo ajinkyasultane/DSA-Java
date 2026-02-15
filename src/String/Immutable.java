a78aaf7ba31c09aa2f7ad9b928868c1cd888a4b5
Ajinkya <ajinkyasultane@gmail.com>
2026-02-15 11:15:25 +0530

    Started Learn String

---
+++ src/String/Immutable.java
@@ -0,0 +1,15 @@
+package String;
+
+public class Immutable {
+    public static void main(String[] args)
+    {
+        String name = "Ajinkya";
+        System.out.println(name);
+
+        String a = "Ajinkya";
+        System.out.println(a);
+
+        a = "Sultane";
+        System.out.println(a);
+    }
+}
