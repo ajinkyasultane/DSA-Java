feaeb2dc1a7e42045377f1fac6c779a531225f82
Ajinkya <ajinkyasultane@gmail.com>
2026-02-22 20:06:29 +0530

    Solve Leet problem no 709

---
+++ src/String/Leet709LowerCase.java
@@ -0,0 +1,15 @@
+package String;
+
+public class Leet709LowerCase
+{
+    public static void main(String[] args)
+    {
+            String name = "AJINKYA";
+        System.out.println(lower(name));
+    }
+    static String lower(String s)
+    {
+      //  s.toLowerCase();
+        return s.toLowerCase();
+    }
+}
