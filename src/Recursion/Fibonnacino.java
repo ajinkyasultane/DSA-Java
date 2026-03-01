9dcb4d78187fe496bc1ce5e33aca2fd0d3b53bd4
Ajinkya <ajinkyasultane@gmail.com>
2026-03-02 01:36:19 +0530

    solve leet 567 no problem as well as learn fibonacci using recursion

---
+++ src/Recursion/Fibonnacino.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class Fibonnacino {
+    public static void main(String[] args)
+    {
+        System.out.println(fibo(3));
+    }
+    static  int fibo(int n)
+    {
+        if(n < 2)
+        {
+            return n;
+        }
+        return  fibo(n-1) + fibo(n -2);
+    }
+}
