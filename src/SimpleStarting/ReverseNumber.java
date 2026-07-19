be062889b226aeaa73e3aaed2290edfc441e6a7d
Ajinkya <ajinkyasultane@gmail.com>
2026-07-19 19:00:05 +0530

    reverse number

---
+++ src/SimpleStarting/ReverseNumber.java
@@ -0,0 +1,18 @@
+package SimpleStarting;
+
+public class ReverseNumber {
+    public static void main(String[] args)
+    {
+        int n = 43335554;
+        int ans = 0 ;
+        while(n > 0)
+        {
+            int rem = n % 10;
+            n /= 10;
+             ans = ans * 10 + rem ;
+
+
+        }
+        System.out.println(ans);
+    }
+}
